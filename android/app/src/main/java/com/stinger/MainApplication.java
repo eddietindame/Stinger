622000e27f105247639cb533fd2ce95776f8aab6
Eddie Tindame <eddietindame@Eddies-MacBook-Pro.local>
2018-02-05 23:02:29 +0000

    Fixed Android build errors

--- android/app/src/main/java/com/stinger/MainApplication.java
+++ android/app/src/main/java/com/stinger/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.soloader.SoLoader;
 import com.facebook.FacebookSdk;
 import com.facebook.CallbackManager;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 
 import java.util.Arrays;
 import java.util.List;
