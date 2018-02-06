fff1d29cb1e68b13fad2957a6b638e16b9c904e9
Eddie Tindame <eddietindame@Eddies-MacBook-Pro.local>
2018-02-06 17:06:01 +0000

    Saving authentication in state

--- src/App.js
+++ src/App.js
@@ -6,21 +6,72 @@ import {
   View
 } from 'react-native';
 import firebase from 'react-native-firebase';
+import { AccessToken, LoginManager } from 'react-native-fbsdk';
 import { Button } from 'react-native-elements';
-import facebookLogin from './modules/FacebookLogin';
 
 export default class App extends Component<{}> {
 
   constructor() {
     super();
+
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+
     this.state = {
-      isAuthenticated: false,
+      user: null
     };
+  }
+
+  componentDidMount() {
+    // Checks if user was previously signed in
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
+      } 
+    });
+  }
+
+  login() {
+    return LoginManager
+      .logInWithReadPermissions(['public_profile', 'email'])
+      .then(result => {
+        if (!result.isCancelled) {
+          console.log(`Login success with permissions: ${result.grantedPermissions.toString()}`)
+          // get the access token
+          return AccessToken.getCurrentAccessToken()
+        }
+      })
+      .then(data => {
+        if (data) {
+          // create a new firebase credential with the token
+          const credential = firebase.auth.FacebookAuthProvider.credential(data.accessToken)
+          // login with credential
+          return firebase.auth().signInWithCredential(credential)
+        }
+      })
+      .then(currentUser => {
+        if (currentUser) {
+          // console.info(JSON.stringify(currentUser.toJSON()))
+          this.setState({ user });
+          console.info(currentUser);
+        }
+      })
+      .catch(error => {
+        console.log(`Login fail with error: ${error}`)
+      })
+  }
+
+  logout() {
+    firebase.auth().signOut()
+      .then(() => {
+        this.setState({ user: null });
+      });
   }
 
   render() {
     return (
       <View style={ styles.container }>
+      { !this.state.user ?
         <Button
           large
           rounded
@@ -28,7 +79,17 @@ export default class App extends Component<{}> {
           icon={{ name: 'facebook-f',
                   type: 'font-awesome' }}
           title='Log in with Facebook'
-          onPress={ facebookLogin } />
+          onPress={ this.login } />
+        :
+        <Button
+          large
+          rounded
+          backgroundColor='#ff0000'
+          icon={{ name: 'facebook-f',
+                  type: 'font-awesome' }}
+          title='Log out'
+          onPress={ this.logout } />
+      }
       </View>
     );
   }
