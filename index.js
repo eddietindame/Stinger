2656a00eda4abb82d5b2529259bffaef7f26436f
Eddie Tindame <eddietindame@Eddies-MacBook-Pro.local>
2018-02-10 03:50:27 +0000

    Implemented Redux logger

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { AppRegistry } from 'react-native';
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 import App from './src/App';
 import reducers from './src/reducers/index';
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(logger));
 
 const Stinger = () => (
     <Provider store={ store }>
