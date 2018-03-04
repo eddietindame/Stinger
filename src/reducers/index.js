0c7dfe3e2945431f0bcc1546a4f953b5e80fae16
Eddie Tindame <eddietindame@Eddies-MacBook-Pro.local>
2018-03-04 19:25:36 +0000

    Reset store on logout

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,20 @@ import routeReducer from './routeReducer'
 import authReducer from './authReducer'
 import dbReducer from './dbReducer'
 import swiperReducer from './swiperReducer'
+import { LOGOUT_SUCCESS } from '../modules/constants'
 
-export default combineReducers({
+const rootReducer = combineReducers({
     authentication: authReducer,
     db: dbReducer,
     route: routeReducer,
     slide: swiperReducer
 })
+
+// If logout action is dispatched, clear store
+export default (state, action) => {
+    if (action.type === LOGOUT_SUCCESS) {
+      state = undefined
+    }
+
+    return rootReducer(state, action)
+}
